bb77f08e6126b3152732ce266a12a20e7a2bcb67
jcoop47 <jtc0047@uah.edu>
2024-02-28 13:45:57 -0600

    Adds user, admin, teller, and customer classes. Not all methods are complete.

---
+++ src/main/backend/admin.java
@@ -0,0 +1,31 @@
+public class Admin extends User{
+    // createTellerAccount
+    // Creates teller accounts.
+    public void createTellerAccount(String id){
+
+    }
+
+    // deleteTellerAccount
+    // Deletes teller accounts.
+    public void deleteTellerAccount(String id){
+
+    }
+
+    // modifyTellerAccount
+    // Changes information of the given telller.
+    public void modifyTellerAccount(String id){
+
+    }
+
+    // viewTellerAccount
+    // Shows teller account info of the given teller.
+    public void viewTellerAccount(String id){
+
+    }
+
+    // viewCustomerBankAccount
+    // Shows customer account info.
+    public void viewCustomerBankAccount(String id){
+
+    }
+}
