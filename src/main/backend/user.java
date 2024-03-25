43d8aa65f4c3a15858bd86843d1bbb49d8dea19a
jcoop47 <jtc0047@uah.edu>
2024-03-25 15:16:08 -0500

    The set methods in user can update the database.

--- src/main/backend/user.java
+++ src/main/backend/user.java
@@ -46,17 +46,21 @@ public class User{
     // setUsername
     // Changes the user's username to the given parameter.
     // Ensures the username is unique and (optionally) check for inappropriate names.
-    public int setUsername(String user){
+    public int setUsername(String user, String db){
         // Still needs to check if it is unique
 
-        if (user.len() < 4 || user.len() > 12){  // checks if username is in the length bounds [4, 12]
+        if (user.length() < 4 || user.length() > 12){  // checks if username is in the length bounds [4, 12]
             return 1;
         }
 
-        // Possibly add list of inappropriate usernames to block.
-        // Needs to be careful, like "hello" shouldn't be blocked.
+        if(updateDatabase(this.getId(), "username", user, db)){  //updates the database
+            username = user;  // sets the username  //sets the current object
+        }
+        else{  //if there was an error updating the database
+            System.out.println("SSN error");
+        }
 
-        username = user;  // sets the username
+
         return 0;  // successful change
     }
 
@@ -64,7 +68,7 @@ public class User{
     // Changes the user's password to the given parameter.
     // Ensures the password is strong by making it 8 characters long, has a number, a special character,
     // and one uppercase and lowercase letter.
-    public int setPassword(String pass){
+    public int setPassword(String pass, String db){
         if (pass.length() < 8){  // checks for proper password strength
             return 1;
         }
@@ -103,8 +107,14 @@ public class User{
         }
 
         if (number && specialChar && upper && lower){  // if all conditions are met
-            password = pass;  // changes the password
-            return 0;  // returns success code
+            if(updateDatabase(this.getId(), "password", pass, db)){  //updates the database
+                password = pass;  //sets the current object
+            }
+            else{  //if there was an error updating the database
+                System.out.println("Password error");
+                return 1; //error with password
+            }
+            return 0;
         }
 
         return 1;  // if not all conditions are met
@@ -112,14 +122,33 @@ public class User{
 
     // setFirstName
     // Changes the user's first name to the given parameter.
-    public void setFirstName(String first){
-        firstName = first;
+    public void setFirstName(String first, String db){
+        if(updateDatabase(this.getId(), "firstname", first, db)){  //updates the database
+            firstName = first;  //sets the current object
+        }
+        else{  //if there was an error updating the database
+            System.out.println("First name error");
+        }
     }
 
     // setLastName
     // Changes the user's last name to the given parameter.
-    public void setLastName(String last){
-        lastName = last;
+    public void setLastName(String last, String db){
+        if(updateDatabase(this.getId(), "lastname", last, db)){  //updates the database
+            lastName = last;  //sets the current object
+        }
+        else{  //if there was an error updating the database
+            System.out.println("Last name error");
+        }
+    }
+
+    public void setId(String num, String db){
+        if(updateDatabase(this.getId(), "customer_id", num, db)){  //updates the database
+            id = num;  //sets the current object
+        }
+        else{  //if there was an error updating the database
+            System.out.println("Last name error");
+        }
     }
 
     // login
@@ -138,13 +167,37 @@ public class User{
         else{  // if just the password is incorrect
             return "Invalid password.";
         }
-
-        return "Login error.";  // if something goes terribly wrong with Java logic
     }
 
     // logout
     // Logs the user out of the system.
     public void logout(){
 
+    }
+
+    // updateDatabase
+    // If the parent class needs to update the database for a subclass.
+    // Used when the subclass doesn't have the set methods for the parent field.
+    private boolean updateDatabase(String row, String col, String field, String db){
+        String url = "jdbc:mysql://localhost:3306/laravel_api";  //database url
+        String user = "root";  //database username
+        String pass = "kd(S(MavJCoLV1";  //database password
+
+        //the sql statement to send to the database
+        String sql = "update " + db + " set " + col + " = '" + field + "' where customer_id = " + row;
+
+        //attempts to connect the driver to the database
+        try (
+                Connection connection = DriverManager.getConnection(url, user, pass);
+                Statement statement = connection.createStatement();
+        )
+        {
+            statement.executeUpdate(sql); //executes the sql statement if the driver connected
+            return true;  //returns success
+        }
+
+        catch (SQLException e) {  //if the driver could not connect
+            return false;  //returns not successful with the connection
+        }
     }
 }
