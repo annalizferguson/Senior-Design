082b8d6bb20bb57686560de099ed0a2fd622e3c4
jcoop47 <jtc0047@uah.edu>
2024-04-14 16:49:07 -0500

    Adds interest income to the database.

--- src/main/backend/FinancialAccount.java
+++ src/main/backend/FinancialAccount.java
@@ -1,12 +1,16 @@
 package main.backend;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.sql.*;
 
 public class FinancialAccount {
     private String id;
     private String type;
     private double balance;
     private int accountNumber;
+    private String customerID;
 
     FinancialAccount() {
         id = "";
@@ -30,24 +34,24 @@ public class FinancialAccount {
     public double getBalance() {
         return balance;
     }
+    public String getCustomerID(){ return customerID; }
 
-    public void zeroOutBalance() {
-        balance = 0;
-    }
+    public void zeroOutBalance() { balance = 0; }
+
 
 
     /**
      * creates new financial account and adds it to database
-     *
      * @param newId
      * @param newType
      * @param newBalance
      */
-    public void create(String newId, String newType, double newBalance) {
+    public void create(String newId, String newType, double newBalance, String cID) {
         id = newId;
         type = newType;
         balance = newBalance;
         accountNumber = 0; // thinking we could random generate this?
+        customerID = cID;
     }
 
     /**
@@ -64,13 +68,15 @@ public class FinancialAccount {
         balance += amount;
         // rounds balance to UP two decimal spaces
         balance = BigDecimal.valueOf(balance).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+        updateTransaction(amount, balance, "Deposit");
+
         return 0;
     }
 
     /**
      * withdraws money from account
      * returns 0 for success, 1 for not enough money
-     *
      * @param amount
      * @return
      */
@@ -79,9 +85,68 @@ public class FinancialAccount {
             System.out.println("Not enough money in account to withdraw this amount.");
             return 1;
         } else {
-            balance -= amount;
-            return 0;
+            if(updateTransaction(amount, balance - amount, "Withdraw")){
+                balance -= amount;
+                return 0;
+            }
+            return 1;
         }
     }
 
+    // updateTransaction
+    // Updates the transactions table and the given financial account table.
+    protected boolean updateTransaction(double amount, double newBalance, String transType){
+        Date currentDate = new Date();  //used to store the current date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("mm-dd-yyyy");  //US date format
+        String date = dateFormat.format(currentDate);  //holds the date in a string
+
+        String db = "";
+
+        if(this.type.equals("Checking")){  //checks for each account type to see which database to store into
+            db = "checking_acc";
+        }
+
+        else if(this.type.equals("Savings")){
+            db = "savings_acc";
+        }
+
+        else if(this.type.equals("Money Market")){
+            db = "moneymarket_acc";
+        }
+
+        else if(this.type.equals("Credit Card")){
+            db = "creditcard_acc";
+        }
+
+        else if(this.type.equals("Home Mortgage")){
+            db = "homemortgage_acc";
+        }
+
+        else{
+            System.out.println("Error with type in updateTransaction: " + this.type);
+            return false;
+        }
+
+        String url = "jdbc:mysql://localhost:3306/laravel_api";  //database url
+        String user = "root";  //database username
+        String pass = "kd(S(MavJCoLV1";  //database password
+
+        String sql1 = "update " + db + " set balance = " + newBalance + " where customer_id = '" + customerID + "'";  //needs work
+        String sql2 = "insert into transactions (customer_id, transaction_type, account_type, amount, new_balance, " +
+                "transaction_date) values ('" + customerID + "', '" + transType + "', '" + type + "', " + amount +
+                ", " + newBalance + ", '" + date + "')";
+
+        try(Connection connection = DriverManager.getConnection(url, user, pass);
+            Statement statement = connection.createStatement();
+        )
+        {
+            statement.executeUpdate(sql1);  //updates the account table
+            statement.executeUpdate(sql2);  //adds to the transaction table
+            return true;  //returns success
+        }
+        catch(SQLException e){
+            e.printStackTrace();  //prints error
+            return false;  //returs false, cannot update database
+        }
+    }
 }
--- src/main/backend/InterestTax.java
+++ src/main/backend/InterestTax.java
@@ -1,5 +1,10 @@
 package main.backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class InterestTax {
     private static final String bankName = "Charger United Credit Union";
     private static final String bankAddr = "301 Sparkman Drive\nHuntsville, AL 35899";  // this is UAH's mailing addr
@@ -33,27 +38,57 @@ public class InterestTax {
     }
 
     public void setFirstName(String first){
-        firstName = first;
+        if(updateDatabase(customerId, "firstname", first)){
+            firstName = first;
+        }
+        else{
+            System.out.println("Could not update firstname in interestTax.");
+        }
     }
 
     public void setLastName(String last){
-        lastName = last;
+        if(updateDatabase(customerId, "lastname", last)){
+            lastName = last;
+        }
+        else{
+            System.out.println("Could not update lastname in interestTax.");
+        }
     }
 
     public void setAddress(String addr){
-        address = addr;
+        if(updateDatabase(customerId, "address", addr)){
+            address = addr;
+        }
+        else{
+            System.out.println("Could not update address in interestTax.");
+        }
     }
 
     public void setPhoneNumber(String phone){
-        phoneNumber = phone;
+        if(updateDatabase(customerId, "phone_number", phone)){
+            phoneNumber = phone;
+        }
+        else{
+            System.out.println("Could not update phone number in interestTax.");
+        }
     }
 
     public void setInterestIncome(double interest){
-        interestIncome = interest;
+        if(updateDatabase(customerId, "interest_income", interest)){
+            interestIncome = interest;
+        }
+        else{
+            System.out.println("Could not update interest in interestTax.");
+        }
     }
 
     public void setCustomerId(String cID){
-        customerId = cID;
+        if(updateDatabase(customerId, "phone_number", cID)){
+            customerId = cID;
+        }
+        else{
+            System.out.println("Could not update customer id in interestTax.");
+        }
     }
 
     public String getBankName(){
@@ -86,5 +121,45 @@ public class InterestTax {
 
     public String getCustomerId(){
         return customerId;
+    }
+
+    private boolean updateDatabase(String row, String col, String field) {
+        String url = "jdbc:mysql://localhost:3306/laravel_api";  //database url
+        String user = "root";  //database username
+        String pass = "kd(S(MavJCoLV1";  //database password
+
+        //the sql statement to send to the database
+        String sql = "update interest_info set " + col + " = '" + field + "' where customer_id = " + row;
+
+        //attempts to connect the driver to the database
+        try (
+                Connection connection = DriverManager.getConnection(url, user, pass);
+                Statement statement = connection.createStatement();
+        ) {
+            statement.executeUpdate(sql); //executes the sql statement if the driver connected
+            return true;  //returns success
+        } catch (SQLException e) {  //if the driver could not connect
+            return false;  //returns not successful with the connection
+        }
+    }
+
+    private boolean updateDatabase(String row, String col, double field) {
+        String url = "jdbc:mysql://localhost:3306/laravel_api";  //database url
+        String user = "root";  //database username
+        String pass = "kd(S(MavJCoLV1";  //database password
+
+        //the sql statement to send to the database
+        String sql = "update interest_info set " + col + " = " + field + " where customer_id = " + row;
+
+        //attempts to connect the driver to the database
+        try (
+                Connection connection = DriverManager.getConnection(url, user, pass);
+                Statement statement = connection.createStatement();
+        ) {
+            statement.executeUpdate(sql); //executes the sql statement if the driver connected
+            return true;  //returns success
+        } catch (SQLException e) {  //if the driver could not connect
+            return false;  //returns not successful with the connection
+        }
     }
 }
