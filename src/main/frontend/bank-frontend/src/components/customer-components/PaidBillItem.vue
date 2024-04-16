0d5010424d125b0be3511f1d17bcba6d4e95e0cb
garviasi <122570548+garviasi@users.noreply.github.com>
2024-04-16 17:08:41 -0500

    added a way to view unpaid bills on the teller side

---
+++ src/main/frontend/bank-frontend/src/components/customer-components/PaidBillItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-card
+            width="100%"
+            variant="tonal"
+    >
+        <v-card-title style="background-color: #4097f5"/>
+        <v-container
+                class="d-flex justify-space-between"
+        >
+            <b>Name</b>
+            <div>{{ billName }}</div>
+        </v-container>
+        <v-container
+                class="d-flex justify-space-between"
+        >
+            <b>Date Due</b>
+            <div>{{ billDate }}</div>
+        </v-container>
+        <v-container
+                class="d-flex justify-space-between"
+        >
+            <b>Amount</b>
+            <div style="font-size: 2em">${{ billAmount }}</div>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+import MakeAPaymentComponent from "@/components/customer-components/MakeAPaymentComponent.vue";
+import {useCustomerStore} from "@/states/UserStore.js";
+
+export default {
+    name: "PaidBillItem.vue",
+    components: {MakeAPaymentComponent},
+    props: {
+        bill: {
+            type: Object
+        },
+        customerID: {
+            type: Number
+        }
+    },
+    data: function () {
+        const store = useCustomerStore()
+        return {
+            store: store,
+            dialogActive: false,
+            billName: this.bill.payeeName,
+            billDate: this.bill.dateDue,
+            billAmount: this.bill.amount,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/frontend/bank-frontend/src/components/teller-components/CustomerBillsComponent.vue
+++ src/main/frontend/bank-frontend/src/components/teller-components/CustomerBillsComponent.vue
@@ -2,9 +2,18 @@
     <v-container>
         <v-card height="10%" class="mb-4" style="background-color: #1565c0; color: white">
             <v-card-title class="d-flex justify-space-between">
-                <div>All Upcoming Bills for <b>{{ customerFirstName }} {{ customerLastName }}</b></div>
+                <div>All {{ billsType }} Bills for <b>{{ customerFirstName }} {{ customerLastName }}</b></div>
+                <v-btn
+                        variant="plain"
+                        color="white"
+                        style="font-size:0.65em"
+                        @click="switchCurrentType"
+                >
+                    Show {{ otherBillsType }}
+                </v-btn>
                 <div>
                     <v-btn
+                            v-if="showUnpaidBills"
                             variant="plain"
                             color="white"
                             style="font-size:0.65em"
@@ -35,26 +44,32 @@
                 </div>
             </v-card-title>
         </v-card>
-        <v-container v-if="bills.length === 0" class="d-flex justify-center">
+        <v-container v-if="unpaidBills.length === 0" class="d-flex justify-center">
             No bills found.
         </v-container>
-        <v-container v-if="billsLoaded" style="height: calc(100vh - 165px)"
+        <v-container v-if="billsLoaded && showUnpaidBills" style="height: calc(100vh - 165px)"
                      class="d-flex justify-center flex-wrap overflow-y-auto">
-            <UpcomingPaymentsItem height="50%" width="45%" class="mb-4 mr-4" v-for="(item, index) in bills"
+            <UpcomingPaymentsItem height="50%" width="45%" class="mb-4 mr-4" v-for="(item, index) in unpaidBills"
                                   :bill="item" :customerID="customerID"/>
+        </v-container>
+        <v-container v-if="billsLoaded && !showUnpaidBills" style="height: calc(100vh - 165px)"
+                     class="d-flex justify-center flex-wrap overflow-y-auto">
+            <PaidBillItem height="50%" width="45%" class="mb-4 mr-4" v-for="(item, index) in paidBills"
+                          :bill="item" :customerID="customerID"/>
         </v-container>
     </v-container>
 </template>
 
 <script>
 import UpcomingPaymentsItem from "@/components/customer-components/UpcomingPaymentsItem.vue";
+import PaidBillItem from "@/components/customer-components/PaidBillItem.vue";
 import MakeAPaymentComponent from "@/components/customer-components/MakeAPaymentComponent.vue";
 import {useTellerStore} from "@/states/TellerStore.js";
 import axios from "axios";
 
 export default {
     name: "CustomerBillsComponent.vue",
-    components: {MakeAPaymentComponent, UpcomingPaymentsItem},
+    components: {MakeAPaymentComponent, UpcomingPaymentsItem, PaidBillItem},
     data: () => {
         const tellerStore = useTellerStore()
         return {
@@ -62,20 +77,39 @@ export default {
             customerID: tellerStore.getCustomerID,
             customerFirstName: tellerStore.getCustomerFirstName,
             customerLastName: tellerStore.getCustomerLastName,
-            bills: [],
+            unpaidBills: [],
+            paidBills: [],
             billsLoaded: false,
             dialogActive: false,
+            billsType: "Unpaid",
+            otherBillsType: "Paid",
+            showUnpaidBills: true,
         }
     },
     methods: {
         async loadUnpaidBills() {
             const {data} = await axios.get(`/api/customers/${this.customerID}/unpaidbill`)
-            this.bills = data
+            this.unpaidBills = data
             this.billsLoaded = true
+        },
+        async loadPaidBills() {
+            const {data} = await axios.get(`/api/customers/${this.customerID}/paidbill`)
+            this.paidBills = data
+        },
+        switchCurrentType() {
+            this.showUnpaidBills = !this.showUnpaidBills
+            if (this.showUnpaidBills) {
+                this.billsType = "Unpaid"
+                this.otherBillsType = "Paid"
+            } else {
+                this.billsType = "Paid"
+                this.otherBillsType = "Unpaid"
+            }
         }
     },
     beforeMount() {
         this.loadUnpaidBills()
+        this.loadPaidBills()
     }
 }
 </script>
